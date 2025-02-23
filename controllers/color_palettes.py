3d1593764d2c8f0600d89c96d41b1b622ed0bc46
noasharabi8 <noasharabi8@gmail.com>
2025-02-23 17:25:36 +0200

    update

--- controllers/color_palettes.py
+++ controllers/color_palettes.py
@@ -19,35 +19,24 @@ def create_color_palette():
           schema:
             id: color_palette
             required:
+                - package_name
                 - name
-                - c1
-                - c2
-                - c3
+                - colorsList
             properties:
+                package_name:
+                    type: string
+                    description: The name of the package
                 name:
                     type: string
                     description: The name of the palette
                 description:
                     type: string
                     description: The description of the color palette
-                c1:
-                    type: string
-                    description: color number 1
-                c2:
-                    type: string
-                    description: color number 2
-                c3:
-                    type: string
-                    description: color number 3
-                c4:
-                    type: string
-                    description: color number 4
-                c5:
-                    type: string
-                    description: color number 5
-                c6:
-                    type: string
-                    description: color number 6
+                colorsList:
+                    type: array
+                    description: An array of colors
+                    items:
+                        type: string                   
     responses:
         201:
             description: The color palette was created successfully
@@ -64,36 +53,49 @@ def create_color_palette():
         return jsonify({"error": "Could not connect to the database"}), 500
 
     # Check if the request is valid
-    if not all(key in data for key in ['name', 'description', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6']):
+    if not all(key in data for key in ['package_name', 'name']):
         return jsonify({"error": "Invalid request"}), 400
+    
+    # בדיקה האם colorsList הוא רשימה
+    if not isinstance(data['colorsList'], list):
+        return jsonify({"error": "colorsList must be a list"}), 400
+    
+
+
 
     # Create the color palette item
     color_palette_item = {
         "_id": str(uuid.uuid4()),
         "name": data['name'],
-        "description": data['description'],
-        "c1": data['c1'],
-        "c2": data['c2'],
-        "c3": data['c3'],
-        "c4": data['c4'],
-        "c5": data['c5'],
-        "c6": data['c6']
+        "description": data.get('description', ""),
+        "colorsList": data['colorsList'] 
     }
 
     
     # Insert the color palette into the database
-    package_collection = db["colorsPalette"]
+    package_collection = db[data['package_name']]
+    # בדיקה אם כבר קיים צבע עם אותו שם באותו package
+    existing_palette = package_collection.find_one({"name": data['name']})
+    if existing_palette:
+        return jsonify({"error": "A color palette with this name already exists in the package"}), 400
+
     package_collection.insert_one(color_palette_item)
 
     return jsonify({"message": "Color palette created successfully", '_id': color_palette_item['_id']}), 201
 
 
-# 2. Get all colors palette's name
-@color_palette_blueprint.route('/color-palette', methods=['GET'])
-def get_all_colors_names():
+# 2. Get palette names for package name
+@color_palette_blueprint.route('/color-palette/<package_name>', methods=['GET'])
+def get_palette_names(package_name):
     """
     Get all colors palette names
     ---
+    parameters:
+        - name: package_name
+          in: path
+          type: string
+          required: true
+          description: Name of the package
     responses:
         200:
             description: List of all color palette names    
@@ -102,7 +104,7 @@ def get_all_colors_names():
     if db is None:
         return jsonify({'error': 'Database not initialized'}), 500
 
-    package_collection = db["colorsPalette"]
+    package_collection = db[package_name]
 
     # שליפת רק את השדה 'name' מכל האובייקטים
     colors_names = list(package_collection.find({}, {"name": 1, "_id": 0}))
@@ -114,13 +116,18 @@ def get_all_colors_names():
 
 
 # 3. Get a color palette by name
-@color_palette_blueprint.route('/color-palette/<color_name>', methods=['GET'])
-def get_color_palette_details(color_name):
+@color_palette_blueprint.route('/color-palette/<package_name>/<palette_name>', methods=['GET'])
+def get_color_palette_details(package_name, palette_name):
     """
     Get details of a specific color palette by name
     ---
     parameters:
-        - name: color_name
+        - name: package_name
+          in: path
+          type: string
+          required: true
+          description: Name of the package    
+        - name: palette_name
           in: path
           type: string
           required: true
@@ -135,10 +142,10 @@ def get_color_palette_details(color_name):
     if db is None:
         return jsonify({'error': 'Database not initialized'}), 500
 
-    package_collection = db["colorsPalette"]  # מסד נתונים קבוע מראש
+    package_collection = db[package_name]  # מסד נתונים קבוע מראש
 
     # חיפוש הרשומה לפי name (Case-Sensitive)
-    palette = package_collection.find_one({"name" : color_name})
+    palette = package_collection.find_one({"name" : palette_name})
 
     if not palette:
         return jsonify({'error': 'Color palette not found'}), 404
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from controllers.myTable import color_palette_blueprint
+from controllers.color_palettes import color_palette_blueprint
 
 def initial_routes(app):
     app.register_blueprint(color_palette_blueprint)
